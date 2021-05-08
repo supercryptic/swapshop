e6aa57c69ca5c5ff03d3edf5dbf91a9676e04807
Altaergo <73134512+Altaergo@users.noreply.github.com>
2021-05-08 23:59:10 +0300

    update main contract

--- contracts/Swappair.sol
+++ contracts/Swappair.sol
@@ -1,15 +1,48 @@
+// SPDX-License-Identifier: MIT
 pragma solidity 0.8.4;
+import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
-contract Swappair {
+contract Swappair is IERC721Receiver {
 
-    address[]memory path = new address [](2)
-    path[0]= NFT1
-    path[1]= NFT2 
+bytes32 [] public override allMarkets;
 
-    function createNFTMarket( NFTA, NFTB)
+mapping(bytes32 => address) public NFTHashToOwners;
+mapping(string => bytes32) public tokenURIsToHash;
+mapping(string => uint) public tokenURIsToTokenId;
+
+event Transfer(from, to, tokenId);
+
+function depositNFT(address NFTOwner, address to, uint tokenId, string memory tokenURI)external {
+safeTransferFrom(msg.sender, address(this), tokenId);
+bytes32 memory nfthash = keccak256(abi.encodePacked('tokenURI'));
+NFTHashToOwners[nfthash] = msg.sender;
+emit Transfer(msg.sender, address(this), tokenId)
+}
+
+function createPair(string memory tokenURI1, string memory tokenURI2) internal {
+    bytes32 [] memory path = new bytes32[](2);
+    path[0] = keccak256(abi.encodePacked('tokenURI'));
+    path[1] = keccak256(abi.encodePacked('tokenURI2'));
+    }
+
+//exchanging an NFT
+    function swapNFTMarket( path, string memory tokenURI)
         external
-        returns (address pair)
-    {
-        require(NFTA != NFTB, "Identical NFTs");
+           {
+        require(keccak256(abi.encodePacked(tokenURI)) == path[0]|path[1], "NFT not in path");
+        bytes32 memory senderHash = keccak256(abi.encodePacked(tokenURI)); 
+        senderNFT = HashToTokenURI[senderHash] 
+if (senderHash == path[0]){
+    NFTHashToOwner[path[1]]
+safeTransfer()
+}
+{
+
+}
+
+if!(keccak256(abi.encodePacked('tokenURI')) == NFTHashToOwners[msg.sender]){
+  address memory otherParty = nftHashToOwners[]
+}
     }
 }
--- contracts/Swapshop.sol
+++ contracts/Swapshop.sol
@@ -1,67 +1,67 @@
-pragma solidity 0.8.4;
-// SPDX-License-Identifier: MIT
-import '@openzeppelin/contracts/token/ERC721/IERC721.sol';
-import '@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol';
+// pragma solidity 0.8.4;
+// // SPDX-License-Identifier: MIT
+// import '@openzeppelin/contracts/token/ERC721/IERC721.sol';
+// import '@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol';
 
-contract Swapshop is IERC721Receiver {
+// contract Swapshop is IERC721Receiver {
 
-MINIMUM_EXCHANGE_FEE = 0.001 ether;
-// mapping(address=> NFT)public ownersToNFTs;
+// // MINIMUM_EXCHANGE_FEE == 0.001 ether;
+// // mapping(address=> NFT)public ownersToNFTs;
 
-mapping(bytes32=> NFT) public nftNameToNFT;
+// mapping(bytes32=> NFT) public nftNameToNFT;
 
-mapping(bytes32=>bytes32)public exchangeNftToNFT;
+// mapping(bytes32=>bytes32)public exchangeNftToNFT;
 
-//store various information about the NFT
-struct NFT{
-    bytes32 NFTName;
-    uint tokenId;
-    address ownerAddress;
-}
+// //store various information about the NFT
+// struct NFT{
+//     bytes32 NFTName;
+//     uint tokenId;
+//     address ownerAddress;
+// }
 
-constructor(){
+// constructor(){
 
-}
+// }
 
-//limit order
-function depositNFT(bytes32 calldata _NFTName, bytes32 calldata _NFTToExchange, uint256 _tokenId)public payable{
-    require(msg.value>=MINIMUM_EXCHANGE_FEE, 'Inadequate funds');
-    require(msg.sender == ownerOf(_tokenId), "Not NFT owner");
-safeTransferFrom(msg.sender, address(this),tokenId);
-struct storage nftToTrade = NFT(_NFTName, _tokenId, msg.sender);
-// ownersToNFTs[msg.sender] = nftToTrade;
-nftNameToNFT[_NFTName] = nftToTrade;
-}
+// //limit order
+// function depositNFT(bytes32 calldata _NFTName, bytes32 calldata _NFTToExchange, uint256 _tokenId)public payable{
+//     require(msg.value>=MINIMUM_EXCHANGE_FEE, 'Inadequate funds');
+//     require(msg.sender == ownerOf(_tokenId), "Not NFT owner");
+// safeTransferFrom(msg.sender, address(this),tokenId);
+// // struct storage nftToTrade = NFT(_NFTName, _tokenId, msg.sender);
+// // ownersToNFTs[msg.sender] = nftToTrade;
+// nftNameToNFT[_NFTName] = nftToTrade;
+// }
 
-function defineNFTToTrade(bytes32 calldata _NFTName, bytes32 calldata _NFTToExchange)public{
-require(msg.sender ==ownerOf(nftNameToNFT[_NFTName].ownerAddress), "Not the owner");
-//define NFT you want to trade for
-exchangeNFTToNFT[_NFTName] = _NFTToExchange;
-}
+// function defineNFTToTrade(bytes32 calldata _NFTName, bytes32 calldata _NFTToExchange)public{
+// require(msg.sender ==ownerOf(nftNameToNFT[_NFTName].ownerAddress), "Not the owner");
+// //define NFT you want to trade for
+// exchangeNFTToNFT[_NFTName] = _NFTToExchange;
+// }
 
-function changeNFTToExchange(bytes calldata _NFTName,bytes calldata _newNFTName)public{
-require(ownerOf(_NFTName)==msg.sender, "Not the owner");
-exchangeNFTtoNFT[_NFTName]=_newNFTName;
-}
+// function changeNFTToExchange(bytes calldata _NFTName,bytes calldata _newNFTName)public{
+// require(ownerOf(_NFTName)==msg.sender, "Not the owner");
+// exchangeNFTtoNFT[_NFTName]=_newNFTName;
+// }
 
-function swap(bytes32 calldata _nftNameA, bytes32 calldata _nftNameB) public {
-require(ownerOf(_nftNameA) == msg.sender || ownerOf(_nftNameB) == msg.sender, "Invalid owner");
-//confirm both NFTs have been updated they want to swap for eachother
-require(nftNameToNFT[_nftNameA] == _nftNameB && nftToNFTName[_nftNameB]==_nftNameA, "NFTs not tradeable");
-address memory ownerA = nftNameToNFT[_nftNameA].ownerAddress;
-address memory ownerB = nftNameToNFT[_nftNameB].ownerAddress;
-// ownersToNFTs[ownerA] = nftNameToNFT[_nftNameB];
-// ownersToNFTs[ownerB] = nftNameToNFT[_nftNameA];
-nftNameToNFT[_nftNameA].ownerAddress = ownerB;
-nftNameToNFT[_nftNameB].ownerAddress = ownerA;
-exchangeNFTToNFT[_nftNameB] = null;
-exchangeNFTToNFT[_nftNameA] = null;
-}
+// function swap(bytes32 calldata _nftNameA, bytes32 calldata _nftNameB) public {
+// require(ownerOf(_nftNameA) == msg.sender || ownerOf(_nftNameB) == msg.sender, "Invalid owner");
+// //confirm both NFTs have been updated they want to swap for eachother
+// require(nftNameToNFT[_nftNameA] == _nftNameB && nftToNFTName[_nftNameB]==_nftNameA, "NFTs not tradeable");
+// address memory ownerA = nftNameToNFT[_nftNameA].ownerAddress;
+// address memory ownerB = nftNameToNFT[_nftNameB].ownerAddress;
+// // ownersToNFTs[ownerA] = nftNameToNFT[_nftNameB];
+// // ownersToNFTs[ownerB] = nftNameToNFT[_nftNameA];
+// nftNameToNFT[_nftNameA].ownerAddress = ownerB;
+// nftNameToNFT[_nftNameB].ownerAddress = ownerA;
+// exchangeNFTToNFT[_nftNameB] = null;
+// exchangeNFTToNFT[_nftNameA] = null;
+// }
 
-function withdrawNFT(bytes32 calldata _nftName)public{    
-require(msg.sender == nftNameToNFT[_nftName].ownerAddress, "Invalid owner");
-safeTransferFrom(address(this), msg.sender, tokenId);
-exchangeNFTToNFT[_nftName] = null;
-}
+// function withdrawNFT(bytes32 calldata _nftName)public{    
+// require(msg.sender == nftNameToNFT[_nftName].ownerAddress, "Invalid owner");
+// safeTransferFrom(address(this), msg.sender, tokenId);
+// exchangeNFTToNFT[_nftName] = null;
+// }
 
-}
+// }
