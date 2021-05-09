a48c59f93f8f26d289cecb5cf12a3b1cae9bd99c
Altaergo <73134512+Altaergo@users.noreply.github.com>
2021-05-09 10:28:38 +0300

    complete

--- contracts/Swappair.sol
+++ contracts/Swappair.sol
@@ -1,48 +1,81 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.4;
+
 import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
-contract Swappair is IERC721Receiver {
+contract Swappair is IERC721Receiver, IERC721 {
+    bytes32[] public override allMarkets;
 
-bytes32 [] public override allMarkets;
+    mapping(bytes32 => address) public NFTHashToOwners;
+    mapping(string => bytes32) public tokenURIsToHash;
+    mapping(bytes32 => uint256) public hashToTokenIds;
+    // mapping(string => uint256) public tokenURIsToTokenId;
 
-mapping(bytes32 => address) public NFTHashToOwners;
-mapping(string => bytes32) public tokenURIsToHash;
-mapping(string => uint) public tokenURIsToTokenId;
+    event Transfer(
+        address indexed from,
+        address indexed to,
+        uint256 indexed tokenId
+    );
 
-event Transfer(from, to, tokenId);
-
-function depositNFT(address NFTOwner, address to, uint tokenId, string memory tokenURI)external {
-safeTransferFrom(msg.sender, address(this), tokenId);
-bytes32 memory nfthash = keccak256(abi.encodePacked('tokenURI'));
-NFTHashToOwners[nfthash] = msg.sender;
-emit Transfer(msg.sender, address(this), tokenId)
-}
-
-function createPair(string memory tokenURI1, string memory tokenURI2) internal {
-    bytes32 [] memory path = new bytes32[](2);
-    path[0] = keccak256(abi.encodePacked('tokenURI'));
-    path[1] = keccak256(abi.encodePacked('tokenURI2'));
+    function depositNFT(
+        address NFTOwner,
+        address to,
+        uint256 tokenId,
+        string memory tokenURI
+    ) external {
+        safeTransferFrom(msg.sender, address(this), tokenId);
+        bytes32 memory nfthash = keccak256(abi.encodePacked("tokenURI"));
+        NFTHashToOwners[nfthash] = msg.sender;
+        hashToTokenIds[nfthash] = tokenId;
+        emit Transfer(msg.sender, address(this), tokenId);
     }
 
-//exchanging an NFT
-    function swapNFTMarket( path, string memory tokenURI)
+    // function _createPair(string memory tokenURI1, string memory tokenURI2)
+    //     internal
+    //     returns (bytes32[])
+    // {
+    //     bytes32[] memory path = new bytes32[](2);
+    //     path[0] = keccak256(abi.encodePacked("tokenURI"));
+    //     path[1] = keccak256(abi.encodePacked("tokenURI2"));
+    //     return path;
+    // }
+
+    // function _determineNFTOwner(bytes32[] memory _path)
+    //     internal
+    //     returns (address)
+    // {
+    //     for (uint256 i = 0; i < _path.length; i++) {
+    //         require(NFTHashToOwners[_path[i]], "Not the owner");
+    //         address nftOwner = NFTHashToOwners[_path[i]] == true;
+    //         return nftOwner;
+    //     }
+    // }
+
+    //exchanging an NFT
+    function swapNFTMarket(string memory tokenURI1, string memory tokenURI2)
         external
-           {
-        require(keccak256(abi.encodePacked(tokenURI)) == path[0]|path[1], "NFT not in path");
-        bytes32 memory senderHash = keccak256(abi.encodePacked(tokenURI)); 
-        senderNFT = HashToTokenURI[senderHash] 
-if (senderHash == path[0]){
-    NFTHashToOwner[path[1]]
-safeTransfer()
-}
-{
+    {
+        bytes32[] memory path = new bytes32[](2);
+        path[0] = keccak256(abi.encodePacked("tokenURI"));
+        path[1] = keccak256(abi.encodePacked("tokenURI2"));
+        require(
+            NFTHashToOwners[path[0]] == true | NFTHashToOwners[path[1]] == true,
+            "msg.sender shd be owner of one NFT"
+        );
 
-}
-
-if!(keccak256(abi.encodePacked('tokenURI')) == NFTHashToOwners[msg.sender]){
-  address memory otherParty = nftHashToOwners[]
-}
+        if (NFTHashToOwners[path[0]] == msg.sender) {
+            uint256 newNFT0 = hashToTokenIds[path[1]];
+            safeTransferFrom(address(this), msg.sender, newNFT0);
+            uint256 nftToOtherParty = hashToTokenIds[path[0]];
+            address otherAddress = NFTHashToOwners[path[1]];
+            safeTransferFrom(address(this), otherAddress, nftToOtherParty);
+        } else {
+            uint256 newNFT1 = hashToTokenIds[path[0]];
+            safeTransferFrom(address(this), msg.sender, newNFT1);
+            uint256 nftToOtherParty1 = hashToTokenIds[path[1]];
+            address otherAddress1 = NFTHashToOwners[path[0]];
+            safeTransferFrom(address(this), otherAddress1, nftToOtherParty1);
+        }
     }
 }
