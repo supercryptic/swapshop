066ee95d793d0f43c182a8d880c7ab20ae0f96b2
Altaergo <73134512+Altaergo@users.noreply.github.com>
2021-05-09 20:01:16 +0300

    _approve

--- contracts/Swappair.sol
+++ contracts/Swappair.sol
@@ -3,54 +3,44 @@ pragma solidity 0.8.4;
 
 import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract Swappair is IERC721Receiver, IERC721 {
-    bytes32[] public override allMarkets;
+contract Swappair is IERC721Receiver, ERC721 {
+    bytes32[] public allMarkets;
 
     mapping(bytes32 => address) public NFTHashToOwners;
     mapping(string => bytes32) public tokenURIsToHash;
     mapping(bytes32 => uint256) public hashToTokenIds;
+
     // mapping(string => uint256) public tokenURIsToTokenId;
 
-    event Transfer(
-        address indexed from,
-        address indexed to,
-        uint256 indexed tokenId
-    );
+    // event Transfer(
+    //     address indexed from,
+    //     address indexed to,
+    //     uint256 indexed tokenId
+    // );
 
-    function depositNFT(
-        address NFTOwner,
-        address to,
+    constructor() ERC721("Swap", "SWP") {}
+
+    function onERC721Received(
+        address operator, //my contract address
+        address from, //nftowner
         uint256 tokenId,
+        bytes calldata data,
         string memory tokenURI
-    ) external {
+    ) external override returns (bytes4) {
+        _approve(address(this), tokenId);
         safeTransferFrom(msg.sender, address(this), tokenId);
-        bytes32 memory nfthash = keccak256(abi.encodePacked("tokenURI"));
-        NFTHashToOwners[nfthash] = msg.sender;
-        hashToTokenIds[nfthash] = tokenId;
-        emit Transfer(msg.sender, address(this), tokenId);
+        bytes32 nftHash = keccak256(abi.encodePacked("tokenURI"));
+        NFTHashToOwners[nftHash] = msg.sender;
+        hashToTokenIds[nftHash] = tokenId;
+        // emit Transfer(msg.sender, address(this), tokenId);
     }
 
-    // function _createPair(string memory tokenURI1, string memory tokenURI2)
-    //     internal
-    //     returns (bytes32[])
-    // {
-    //     bytes32[] memory path = new bytes32[](2);
-    //     path[0] = keccak256(abi.encodePacked("tokenURI"));
-    //     path[1] = keccak256(abi.encodePacked("tokenURI2"));
-    //     return path;
-    // }
-
-    // function _determineNFTOwner(bytes32[] memory _path)
-    //     internal
-    //     returns (address)
-    // {
-    //     for (uint256 i = 0; i < _path.length; i++) {
-    //         require(NFTHashToOwners[_path[i]], "Not the owner");
-    //         address nftOwner = NFTHashToOwners[_path[i]] == true;
-    //         return nftOwner;
-    //     }
-    // }
+    function _approve(address to, uint256 tokenId) internal virtual {
+        _tokenApprovals[tokenId] = to;
+        emit Approval(ERC721.ownerOf(tokenId), to, tokenId);
+    }
 
     //exchanging an NFT
     function swapNFTMarket(string memory tokenURI1, string memory tokenURI2)
